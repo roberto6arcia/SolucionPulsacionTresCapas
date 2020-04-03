63dac5e9b0f4e7e2d8202c7ec2c896b6d98e6208
Roberto Garcia <robertoc.garcia99@gmail.com>
2020-04-03 18:46:30 -0500

    first commit

---
+++ Entity/Persona.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Entity
+{
+    public class Persona
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+        public decimal Pulsacion { get; set; }
+        public void CalcularPulsaciones() 
+        {
+            if (Sexo.Equals("F") || Sexo.Equals("f"))
+            {
+                Pulsacion=(220 - Edad) / 10;
+            }
+            else
+            {
+                Pulsacion=(210 - Edad) / 10;
+            }
+        }
+        
+    }
+}
---
+++ pulsacionesdotnet/Models/PersonaModel.cs
@@ -0,0 +1,34 @@
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pulsacionesdotnet.Models
+{
+    public class PersonaModel
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+    }
+
+    public class PersonaViewModel : PersonaInputModel
+    {
+        public PersonaViewModel()
+        {
+
+        }
+        public PersonaViewModel(Persona persona)
+        {
+            Identificacion = persona.Identificacion;
+            Nombre = persona.Nombre;
+            Edad = persona.Edad;
+            Sexo = persona.Sexo;
+            Pulsacion = persona.Pulsacion;
+        }
+        public decimal Pulsacion { get; set; }
+        
+    }
+}
